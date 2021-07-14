cc92527513d8d5cb6e007d572b73e78d0a717d01
ABHRA RAY CHAUDHURI <abhrar12@gmail.com>
2021-07-14 14:25:14 +0530

    my feed

--- app/src/main/java/com/example/wemet/account_profile.java
+++ app/src/main/java/com/example/wemet/account_profile.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.denzcoskun.imageslider.ImageSlider;
-import com.denzcoskun.imageslider.models.SlideModel;
+//import com.denzcoskun.imageslider.ImageSlider;
+//import com.denzcoskun.imageslider.models.SlideModel;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 public class account_profile extends AppCompatActivity {
 
@@ -17,13 +17,12 @@ public class account_profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_profile);
 
-        ImageSlider imageSlider=findViewById(R.id.accountimageslider);
+        /*ImageSlider imageSlider=findViewById(R.id.accountimageslider);
         List<SlideModel> slideModels=new ArrayList<>();
-        slideModels.add(new SlideModel("http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max","Image 1"));
-        slideModels.add(new SlideModel("http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max","Image 2"));
+        slideModels.add(new SlideModel("http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max","Image 1"));slideModels.add(new SlideModel("http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max","Image 2"));
         slideModels.add(new SlideModel("http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max","Image 3"));
         slideModels.add(new SlideModel("http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max","Image 4"));
         slideModels.add(new SlideModel("http://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max","Image 5"));
-        imageSlider.setImageList(slideModels,true);
+        imageSlider.setImageList(slideModels,true);*/
     }
 }
